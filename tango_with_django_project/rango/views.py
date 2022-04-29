8f7c12c8b37539ade41dfbfd893013a6a159b08a
Jose Mendoza <sagaka1g@gmail.com>
2022-04-29 11:33:27 -0500

    actualizacion de las vistas

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango say hey there!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cuypcake!'}
+    #return HttpResponse("Rango say hey there!")
+    return render(request, 'rango/index.html', context = context_dict)
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.")
